3afec0b061982e54977618fd662eae25435d0626
Weslen <weslensevero@hotmail.com>
2024-10-25 08:00:43 -0300

    Header added in bootstrap - Quickly moving components to create a SPA

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 
 <script setup>
-  import moneyHeader from "./components/moneyHeader.vue"
   import moneyPage from "./components/moneyPage.vue";
-  import moneyFooter from "./components/moneyFooter.vue";
 </script>
 
 <template>
-  <moneyHeader/>
   <moneyPage/>
-  <moneyFooter/>
 </template>
--- src/components/moneyPage.vue
+++ src/components/moneyPage.vue
@@ -5,6 +5,7 @@ const currencies = ref([]); // Inicializa como um array vazio para busca na API
 const show = ref(false);
 const apiUrl = 'https://v6.exchangerate-api.com/v6/20c06da9f2139de4f408f101/latest/USD'; 
 
+
 // Função para determinar a manipulação do botão
 function toggleCurrencies() {
   show.value = !show.value; 
@@ -29,8 +30,50 @@ onMounted(fetchCurrencies);
 </script>
 
 <template>
-  <section class="moneyPage">
-      <h2>Here I'll make a new page to show different countries and money options</h2>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+          <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Link
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Link</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    
+    <h1 class="title">MoneyFlow</h1>
+  </header>
+
+  <hr>
+
+  <main class="moneyPage">
+      <h2>Select the currencie of your preference</h2>
       <div class="currencies">
         <button v-if="!show" @click="toggleCurrencies">Show Currencies</button>
         <button v-if="show" @click="toggleCurrencies">Hide Currencies</button>
@@ -39,8 +82,56 @@ onMounted(fetchCurrencies);
           <li v-for="[currency, rate] in currencies" :key="currency">{{ currency }}: {{ rate }}</li>
         </ul>
       </div>
-  </section>
+  </main>
+
+  <footer class="text-center text-lg-start">
+    <div>
+      <ul class="ul_footer">
+        <li>Home</li>
+        <li>FAQ</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+      <p class="copy_footer">Desenvolvido por<a href="https://github.com/weslenmombach" target="_blank" > Weslen Mombach </a> <span>&copy;</span></p>
+  </footer>
 </template>
 
 <style>
+.title{
+  text-align: center;
+}
+
+footer {
+  background-color: grey;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Centraliza todos os elementos filhos horizontalmente */
+}
+
+.ul_footer {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  max-width: 300px;
+  width: 100%; /* Faz o <ul> ocupar 100% de largura */
+  margin: 0 auto; /* Centraliza o <ul> */
+  padding: 0;
+}
+
+.ul_footer > li {
+  margin: 10px;
+}
+
+.copy_footer {
+  text-decoration: none;
+  color: black;
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+.copy_footer >a:hover {
+  text-decoration: underline;
+}
 </style>
