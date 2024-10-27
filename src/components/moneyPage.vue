0bb38149439c24c0d81054494511621e416d5fd4
Weslen <weslensevero@hotmail.com>
2024-10-27 16:03:45 -0300

    Add some styles and card

--- src/components/moneyPage.vue
+++ src/components/moneyPage.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 
 const currencies = ref([]); // Inicializa como um array vazio para busca na API
 const show = ref(false);
-const apiUrl = 'https://v6.exchangerate-api.com/v6/20c06da9f2139de4f408f101/latest/USD'; 
-
+const apiUrl =
+  "https://v6.exchangerate-api.com/v6/20c06da9f2139de4f408f101/latest/USD";
 
 // Função para determinar a manipulação do botão
 function toggleCurrencies() {
-  show.value = !show.value; 
+  show.value = !show.value;
 }
 
 // Função para buscar as cotações
@@ -16,12 +16,12 @@ async function fetchCurrencies() {
   try {
     const response = await fetch(apiUrl); // Usa a constante apiUrl
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
     currencies.value = Object.entries(data.conversion_rates); // Converte o objeto de taxas em um array de pares [moeda, taxa]
   } catch (error) {
-    console.error('Erro ao buscar cotações:', error);
+    console.error("Erro ao buscar cotações:", error);
   }
 }
 
@@ -34,65 +34,114 @@ onMounted(fetchCurrencies);
   <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarScroll">
-          <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <ul
+            class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+            style="--bs-scroll-height: 100px"
+          >
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
 
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Options
               </a>
               <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="#">Action</a></li>
                 <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
               </ul>
             </li>
-
           </ul>
           <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search Currencie" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search Currencie"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
           </form>
         </div>
       </div>
     </nav>
-    
+
     <h1 class="title">MoneyFlow</h1>
   </header>
 
-
-  <main class="moneyPage">
+<main class="moneyPage">
+  <div class="totalContent">
+    <div class="topSections">
       <section class="select_currencies">
-        <h2 class="title_main">Select the currencie</h2>
-        <hr>
+        <h2 class="title_main">Select the currency</h2>
+        <hr />
         <div class="currencies">
           <button v-if="!show" @click="toggleCurrencies">Show Currencies</button>
           <button v-if="show" @click="toggleCurrencies">Hide Currencies</button>
-          <!-- Utilização de toggle para manipular o button -->
           <ul v-if="show">
-            <li v-for="[currency, rate] in currencies" :key="currency">{{ currency }}: {{ rate }}</li>
+            <li v-for="[currency, rate] in currencies" :key="currency">
+              {{ currency }}: {{ rate }}
+            </li>
           </ul>
         </div>
       </section>
-
       <section class="calculator">
         <div class="calculator_page">
-          <h2 class="title_main">
-            Calculate your currencie
-          </h2>
-          <hr>
+          <h2 class="title_main">Calculate your currency</h2>
+          <hr />
         </div>
       </section>
-  </main>
+    </div>
 
-<!--FOOTER-->
+    <section class="menu">
+      <div class="card text-center">
+        <div class="card-header">
+          <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="true" href="#">How to use</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Why to use</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Questions</a>
+            </li>
+          </ul>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</main>
+
+  <!--FOOTER-->
   <footer class="text-center text-lg-start">
     <div>
       <ul class="ul_footer">
@@ -101,53 +150,71 @@ onMounted(fetchCurrencies);
         <li>Contact</li>
       </ul>
     </div>
-    <p class="copy_footer">Desenvolvido por&nbsp;<a href="https://github.com/weslenmombach" target="_blank">Weslen Mombach</a>&nbsp;<span>&copy;</span></p>
-
+    <p class="copy_footer">
+      Desenvolvido por&nbsp;<a
+        href="https://github.com/weslenmombach"
+        target="_blank"
+        >Weslen Mombach</a
+      >&nbsp;<span>&copy;</span>
+    </p>
   </footer>
 </template>
 
 <style>
-*{
+* {
   margin: 0px;
   padding: 0px;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 }
-.title{
+.title {
   text-align: center;
   font-weight: bolder;
   padding: 80px;
 }
 
 /*CONTENT MAIN*/
-.moneyPage{
+.totalContent {
   display: flex;
-  justify-content: space-around;
-  height: 100vh;
-  background: linear-gradient(to top, #0A3D62,#1E90FF);
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  background: linear-gradient(to top, #0a3d62, #1e90ff);
+  min-height: 100vh;
 }
 
-main h2{
+.topSections {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 60%;
+
+  margin-bottom: 20px;
+}
+
+
+main h2 {
   text-align: center;
 }
 
-.select_currencies{
+.menu {
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  max-width: 600px;
+}
+
+.select_currencies, .calculator {
   border: 2px solid white;
   border-radius: 30px;
   box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.5);
   width: 28vw;
-  margin: 40px 0px;
+  margin: 20px 0px; 
+  height: 70vh;
 }
 
-.calculator{
-  border: 2px solid white;
-  border-radius: 30px;
-  box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.5);
-  width: 28vw;
-  margin: 40px 0px;
-}
-
-.title_main{
+.title_main {
   padding-top: 20px;
+  text-align: center;
 }
 
 /* FOOTER */
@@ -158,7 +225,7 @@ footer {
   bottom: 0;
   left: 0;
   width: 100%;
-  background-color: #0A3D62;
+  background-color: #0a3d62;
   color: white;
   padding-top: 100px;
 }
@@ -168,8 +235,8 @@ footer {
   display: flex;
   justify-content: space-between;
   max-width: 300px;
-  width: 100%; 
-  margin: 0 auto; 
+  width: 100%;
+  margin: 0 auto;
   padding: 0;
 }
 
@@ -185,7 +252,7 @@ footer {
   margin-top: 10px;
 }
 
-.copy_footer >a:hover {
+.copy_footer > a:hover {
   text-decoration: underline;
 }
 
@@ -196,7 +263,8 @@ footer {
     align-items: center;
   }
 
-  .select_currencies, .calculator {
+  .select_currencies,
+  .calculator {
     width: 80%;
     height: 100vh;
   }
